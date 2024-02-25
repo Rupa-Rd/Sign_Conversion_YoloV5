b410ad675be9be3964a2bf8f47b6ee68116d14e0
Rupa-Rd <roopa.69.spr@gmail.com>
2024-02-25 12:08:40 +0530

    Real-time prediction file added

---
+++ YOLO_v5/run.py
@@ -0,0 +1,9 @@
+import os
+
+# import pathlib
+# pathlib.PosixPath = pathlib.WindowsPath
+
+
+
+os.system("python yolov5\detect.py --weights best.pt --img 416 --conf 0.035 --source 0")
+#Sign_Conversion_YoloV5\YOLO_v5\yolov5\detect.py
